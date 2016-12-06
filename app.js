d62c2413743c2406a3dc08ec0ef6043a3cb6cf79
Petar Slovic <petar.slovic@gmail.com>
2016-12-06 20:07:55 +0100

    #2 - Add some security to express

--- app.js
+++ app.js
@@ -7,10 +7,14 @@ var mongoose   = require('mongoose');
 var getUserFromUrlHash = require('./middleware/getUserFromUrlHash');
 var GreetingHelper = require('./helpers/greeting-helper');
 mongoose.Promise = require('bluebird');
+var RateLimit = require('express-rate-limit');
+var helmet = require('helmet');
 
 mongoose.connect(process.env.MONGO_DB_URL);
 
 var app = express();
+
+app.enable('trust proxy');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -23,6 +27,13 @@ app.engine('.hbs', exphbs({
 }));
 
 app.set('view engine', '.hbs');
+
+app.use(helmet());
+app.use(new RateLimit({
+  windowMs: 60*1000,
+  max: 100,
+  delayMs: 0
+}));
 
 // app.use(getUserFromUrlHash);
 
